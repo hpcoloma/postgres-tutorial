d62556b112c5c82aa44154bb62bf1623deec5614
holly coloma <holly.life.15@gmail.com>
2024-05-19 21:44:37 +0000

    sqlalchemy tutorial

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -3,17 +3,34 @@ import psycopg2
 #connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
 
-# build a cursos objuect of the database
+# build a cursos object of the database
 cursor = connection.cursor()
 
 # Query 1 - select all records from the "Artist" table
-cursor.execute ('SELECT * FROM "Artist"')
+# cursor.execute ('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only by "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select only by "ArtistId" #51 from the "Artist" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+
 
 # fetch the results (multiplt)
-results = cursos.fetchall()
+# results = cursor.fetchall()
 
 #fetch the result (single)
-# results = cursor.fetchone()
+results = cursor.fetchone()
 
 # close the connection
 connection.close
